2291b5cb00efba05fdcdb1e08aec69fd4eeb0ecf
wail <wailofsuicide@gmail.com>
2019-07-03 14:21:44 -0400

    Update Build.cs to include UMG, remove VR/HMD

---
+++ Source/OpenTournament/Interfaces/UR_HUDMatchTimerInterface.h
@@ -0,0 +1,38 @@
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "UR_HUDMatchTimerInterface.generated.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Interface to Open or Close MatchTimer
+ */
+UINTERFACE(BlueprintType)
+class OPENTOURNAMENT_API UUR_HUDMatchTimerInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+class IUR_HUDMatchTimerInterface
+{
+    GENERATED_BODY()
+
+public:
+
+    /**
+    * Open Match Timer
+    */
+    UFUNCTION()
+    virtual void OpenMatchTimer() PURE_VIRTUAL(IUR_HUDMatchTimerInterface::OpenMatchTimer,);
+
+    /**
+    * Close Match Timer
+    */
+    UFUNCTION()
+    virtual void CloseMatchTimer() PURE_VIRTUAL(IUR_HUDMatchTimerInterface::CloseMatchTimer,);
+};
