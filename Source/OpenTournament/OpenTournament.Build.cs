6be9c485d04a439039e1ecd6e4102ceb69240026
wail <wailofsuicide@gmail.com>
2019-07-07 11:56:41 -0400

    Update Build.cs to include Slate

--- Source/OpenTournament/OpenTournament.Build.cs
+++ Source/OpenTournament/OpenTournament.Build.cs
@@ -8,6 +8,6 @@ public class OpenTournament : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore" });
 	}
 }
