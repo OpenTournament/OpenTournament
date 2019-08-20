20435a2a61d2ab63861b564a300ec3ab23b132c1
Ricardo Cruz <ricardocruz91@outlook.com>
2019-08-20 23:25:24 +0100

    [Added] Basic inventory system that holds the weapons the user picks up. Pickup is working and weapon select as well. Weapon gets attached to pawn and it is removed when a new weapon is selected

---
+++ Source/OpenTournament/UR_InventoryComponent.h
@@ -0,0 +1,46 @@
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/Components/ActorComponent.h"
+
+#include "OpenTournament.h"
+#include "UR_Weapon.h"
+
+#include "UR_InventoryComponent.generated.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Forward declaration
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * InventoryComponent is the base component for use by actors to have an inventory.
+ */
+UCLASS(DefaultToInstanced, BlueprintType, meta = (Tooltip = "A InventoryComponent is a reusable component that can be added to any actor to give it a Inventory value.", ShortTooltip = "A InventoryComponent is a reusable component that can be added to any actor to give it a Inventory value."), hideCategories = (UR, Character, Collision, Cooking))
+class OPENTOURNAMENT_API UUR_InventoryComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+
+    UUR_InventoryComponent();
+
+	TArray<AUR_Weapon*> InventoryW;
+	AUR_Weapon * ActiveWeapon;
+
+	void Add(AUR_Weapon* weapon);
+
+	void ShowInventory();
+
+	int32 SelectWeapon(int number);
+
+	
+    
+};
--- Source/OpenTournament/UR_Weapon.h
+++ Source/OpenTournament/UR_Weapon.h
@@ -3,11 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/PrimitiveComponent.h"
+#include "Components/ShapeComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 #include "GameFramework/Actor.h"
+#include "UR_Character.h"
+#include "Engine/Canvas.h" // for FCanvasIcon
 
 #include "UR_Weapon.generated.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
+
+class AUR_Weapon;
+
+namespace EWeaponState
+{
+	enum Type
+	{
+		Idle,
+		Firing,
+		Equipping,
+	};
+}
 
 
 /**
@@ -17,17 +35,132 @@ UCLASS()
 class OPENTOURNAMENT_API AUR_Weapon : public AActor
 {
 	GENERATED_BODY()
-	
+
+	enum class EAmmoType
+	{
+		EBullet,
+		ERocket,
+		EMax,
+	};
+
+	virtual void PostInitializeComponents() override;
+
+	virtual void Destroyed() override;
+
+
+	/** add ammo */
+	void GiveAmmo(int AddAmount);
+
+	/** consume a bullet */
+	void UseAmmo();
+
+	/** query ammo type */
+	virtual EAmmoType GetAmmoType() const
+	{
+		return EAmmoType::EBullet;
+	}
+
+	/** start weapon fire */
+	virtual void StartFire();
+
+	/** stop weapon fire */
+	virtual void StopFire();
+
+	/** check if weapon can fire */
+	bool CanFire() const;
+
+
 public:	
 	// Sets default values for this actor's properties
-	AUR_Weapon();
+	AUR_Weapon(const FObjectInitializer & ObjectInitializer);
 
-protected:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	AUR_Character* PlayerController;
+
+	UShapeComponent* Tbox;
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* SM_TBox;
+
+	UPROPERTY(EditAnywhere)
+	FString WeaponName = FString(TEXT(""));
+
+	void Pickup();
+
+	void setEquipped(bool eq);
+
+	/** weapon is being equipped by owner pawn */
+	virtual void OnEquip(AUR_Weapon* LastWeapon);
+
+	/** weapon is holstered by owner pawn */
+	virtual void OnUnEquip();
+
+	/** check if it's currently equipped */
+	bool IsEquipped() const;
+
+	/** check if mesh is already attached */
+	bool IsAttachedToPawn() const;
+
+	/** attaches weapon mesh to pawn's mesh */
+	void AttachMeshToPawn();
+
+	/** detaches weapon mesh from pawn */
+	void DetachMeshFromPawn();
+
+	void GetPlayer(AActor* Player);
+
+	bool bItemIsWithinRange = false;
+
+	bool equipped = false;
+
+	UFUNCTION()
+		void OnTriggerEnter(class UPrimitiveComponent* HitComp, class AActor* Other, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnTriggerExit(class UPrimitiveComponent* HitComp, class AActor* Other, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	/** get current weapon state */
+	EWeaponState::Type GetCurrentState() const;
+
+	/** get current ammo amount (total) */
+	int32 GetCurrentAmmo() const;
+
+	/** get max ammo amount */
+	int32 GetMaxAmmo() const;
+
+	/** get weapon mesh (needs pawn owner to determine variant) */
+	USkeletalMeshComponent* GetWeaponMesh() const;
+
+	/** get pawn owner */
+	UFUNCTION(BlueprintCallable, Category = "Game|Weapon")
+		class AUR_Character* GetPawnOwner() const; //TODO: Check this
+
+	/** set the weapon's owning pawn */
+	void SetOwningPawn(AUR_Character* AURCharacter);
+
+	private:
+
+	/** weapon mesh: 1st person view */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USkeletalMeshComponent* Mesh1P;
+
+	/** weapon mesh: 3rd person view */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USkeletalMeshComponent* Mesh3P;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Weapon usage
+
+	protected:
+	/** Returns Mesh1P subobject **/
+	FORCEINLINE USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns Mesh3P subobject **/
+	FORCEINLINE USkeletalMeshComponent* GetMesh3P() const { return Mesh3P; }
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
