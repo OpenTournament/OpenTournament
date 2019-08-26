b45fb8bf50daf4adf21d15880179adc3f83fa104
Ricardo Cruz <ricardocruz91@outlook.com>
2019-08-26 17:59:24 +0100

    Added simple damage dealing

--- Source/OpenTournament/UR_Projectile_Sniper.cpp
+++ Source/OpenTournament/UR_Projectile_Sniper.cpp
@@ -2,6 +2,7 @@
 
 
 #include "UR_Projectile_Sniper.h"
+#include "Engine.h"
 
 // Sets default values
 AUR_Projectile_Sniper::AUR_Projectile_Sniper(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
@@ -12,4 +13,33 @@ AUR_Projectile_Sniper::AUR_Projectile_Sniper(const FObjectInitializer& ObjectIni
 	ProjectileMovementComponent->InitialSpeed = ProjectileMovementComponent->InitialSpeed*8;
 	ProjectileMovementComponent->MaxSpeed = ProjectileMovementComponent->InitialSpeed * 8;
 
+	Particles = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Particles"));
+	Particles->SetRelativeLocation(FVector::ZeroVector);
+	Particles->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticlesInAssets(TEXT("ParticleSystem'/Game/SciFiWeapDark/FX/Particles/P_SniperRifle_Tracer_Light.P_SniperRifle_Tracer_Light'"));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticlesInAssets2(TEXT("ParticleSystem'/Game/SciFiWeapDark/FX/Particles/P_Impact_Stone_Medium_Light.P_Impact_Stone_Medium_Light'"));
+
+	trail = ParticlesInAssets.Object;
+	impact = ParticlesInAssets2.Object;
+
+	Particles->SetTemplate(ParticlesInAssets.Object);
+
+}
+
+// Called when the game starts or when spawned
+void AUR_Projectile_Sniper::BeginPlay()
+{
+	Super::BeginPlay();
+	CollisionComponent->OnComponentHit.AddDynamic(this, &AUR_Projectile_Sniper::OnHit);
+	CollisionComponent->SetGenerateOverlapEvents(true);
+}
+
+void AUR_Projectile_Sniper::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Particles->SetTemplate(impact);
+	OtherActor->TakeDamage(70, FDamageEvent::FDamageEvent(), NULL, this);
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Damage Event by SNIPER RIFLE 1")));
+	ProjMesh->DestroyComponent();
+	DestroyAfter(3);
 }
