9222e01d83148ce1d06d522e9b3e1266c60cf03a
HopeMetal <liudasdy@gmail.com>
2019-08-28 16:35:42 +0300

    Fix for previous commit failing. Hopefully.

--- Source/OpenTournament/UR_PlayerInput.cpp
+++ Source/OpenTournament/UR_PlayerInput.cpp
@@ -3,3 +3,28 @@
 
 #include "UR_PlayerInput.h"
 
+void UUR_PlayerInput::SayHello()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UR_PlayerInput says Hello."));
+}
+
+/*
+void UUR_PlayerInput::SetActionKeyMappingKey(FInputActionKeyMapping & ActionKeyMapping, FKey Key)
+{
+	ActionKeyMapping.Key = Key;
+}
+*/
+
+void UUR_PlayerInput::SetActionKeyMappingKey(const FInputActionKeyMapping ActionKeyMapping, FKey Key)
+{
+	int FoundIndex = -1;
+	if (ActionMappings.Find(ActionKeyMapping, FoundIndex))
+	{
+		FInputActionKeyMapping & KeyMappingToModify = ActionMappings[FoundIndex];
+		KeyMappingToModify.Key = Key;
+		ForceRebuildingKeyMaps(false);
+		//SaveConfig();
+	}
+	else
+		return;
+}
