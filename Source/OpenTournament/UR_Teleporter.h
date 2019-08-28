536d7ff7381433850b1a329abffcd2436f303f7d
wail <wailofsuicide@gmail.com>
2019-08-27 23:16:41 -0400

    Updates for Teleporter part 2
    - Support actors generically, not just Characters
    - Teleport functionality works
    - Added more default components
    - Default Subcomponent location is broken WIP

--- Source/OpenTournament/UR_Teleporter.h
+++ Source/OpenTournament/UR_Teleporter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "UR_Teleporter.generated.h"
 
+class UArrowComponent;
+class UAudioComponent;
 class UCapsuleComponent;
 class UStaticMeshComponent;
 
@@ -25,23 +27,54 @@ public:
     // Sets default values for this actor's properties
     AUR_Teleporter(const FObjectInitializer& ObjectInitializer);
 
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Exit Properties")
-    AUR_Teleporter* DestinationTeleporter;
+    /*
+    * Static Mesh Component - Teleporter Base
+    */
+    UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Mesh")
+    UStaticMeshComponent* BaseMeshComponent;
 
+    /*
+    * Capsule Component - Active Teleport Region
+    */
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Capsule")
+    UCapsuleComponent* BaseCapsule;
+
+    /*
+    * Audio Component
+    */
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Audio")
+    UAudioComponent* AudioComponent;
+
+    /*
+    * Arrow Component
+    */
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Arrow")
+    UArrowComponent* ArrowComponent;
+
+    /*
+    * ParticleSystem Component
+    */
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Particle")
+    UParticleSystemComponent* ParticleSystemComponent;
+
+
+    /*
+    * Destination of Teleport - May be another Teleporter, TargetPoint, etc.
+    */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Exit Properties")
+    AActor* DestinationActor;
+
+    /*
+    * Does our teleport force us into a new rotation, or is it relative to our DestinationActor's rotation?
+    */
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Exit Properties")
     EExitRotation ExitRotationType = EExitRotation::Relative;
 
+    /*
+    * Do Actors teleported retain their velocity?
+    */
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Exit Properties")
-    FVector2D ExitVector;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Exit Properties")
-    bool KeepMomentum = true;
-
-    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Mesh")
-    UStaticMeshComponent* BaseMeshComponent;
-
-    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Capsule")
-    UCapsuleComponent* BaseCapsule;
+    bool bKeepMomentum = true;
 
 
 protected:
@@ -51,8 +84,6 @@ protected:
 public:
     // Called every frame
     virtual void Tick(float DeltaTime) override;
-
-    float GetExitHeading();
 
     bool PerformTeleport(AActor* TargetActor);
 
