407770fe631bc0f094edbeb8256862189267990b
chatouille@work <ut4.btdevteam@gmail.com>
2019-10-21 13:48:54 +0200

    Added an options menu that is just a wrapper for keybindings menu atm. Options menu can be opened in main menu. Added ingame menu. Ingame menu can also open options, or go back to main menu, or exit to desktop.
    - Implemented basic menu navigation in BaseMenu base class.
    - Ingame menu content in UI/IngameMenu.
    - Options menu content in UI/Options.
    - Re-added utilities 'IsKeyMappedToAction', might cause merge conflict with chat branch.
    - Made chat history a BaseMenu subclass, ping me for merge issues as well.
    - Small chatbox fix, was unintentionally resetting input mode to GameOnly on construct.
    - Added action ShowIngameMenu, re-added missing BeginSay BeginTeamSay ShowChatHistory.
    
    Stuff to do :
    - Rework main menu, remove background art, use menu map. Move into UI/MainMenu.
    - Use/rework main menu classes for better control (MainMenuGM, MainMenuPC).
    - PC should set input mode GameOnly on begin play / after load. Right now doing this from main menu when click play.
    
    I need to pull changes merged into develop/ before going further.

--- Source/OpenTournament/Widgets/UR_Widget_BaseMenu.cpp
+++ Source/OpenTournament/Widgets/UR_Widget_BaseMenu.cpp
@@ -3,8 +3,92 @@
 
 #include "UR_Widget_BaseMenu.h"
 
-void UUR_Widget_BaseMenu::OpenMenu()
+#include "WidgetBlueprintLibrary.h"
+#include "UR_FunctionLibrary.h"
+
+UUR_Widget_BaseMenu::UUR_Widget_BaseMenu(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
-	AddToViewport();
+	ParentMenu = nullptr;
+
+	bAutoPause = true;
+	bPausedByDialog = false;
+
+	bCloseOnEscape = true;
+	CloseOnAction = TEXT("");
+
+	// We should never let focus go back to parent widget or we are doomed.
+	// In case we are making a dialog/popup -like thing (not full screen),
+	// make sure to at least add a full screen invisible border in the background.
+	// In these cases anyways it is good practice to cover the background with a shade at least.
+	bIsFocusable = true;
 }
 
+void UUR_Widget_BaseMenu::Show(UWidget* InParentMenu)
+{
+	ParentMenu = InParentMenu;
+
+	AddToViewport(10);	// base all menus on a higher zOrder so they don't conflict with hud widgets (chatbox...)
+
+	UWidgetBlueprintLibrary::SetInputMode_UIOnlyEx(GetOwningPlayer(), this);	//<-- should take focus
+
+	if (!IsValid(ParentMenu))
+	{
+		if (bAutoPause && !GetWorld()->IsPaused())
+		{
+			bPausedByDialog = GetOwningPlayer()->SetPause(true);
+		}
+	}
+}
+
+void UUR_Widget_BaseMenu::Close(bool bForce)
+{
+	if (!bForce && PreventClose())
+		return;
+
+	if (!IsValid(ParentMenu))
+	{
+		UWidgetBlueprintLibrary::SetInputMode_GameOnly(GetOwningPlayer());
+
+		if (bPausedByDialog)
+		{
+			GetOwningPlayer()->SetPause(false);
+			bPausedByDialog = false;
+		}
+	}
+	else
+	{
+		ParentMenu->SetUserFocus(GetOwningPlayer());
+	}
+
+	RemoveFromParent();
+
+	OnMenuClosed.Broadcast();
+}
+
+bool UUR_Widget_BaseMenu::PreventClose_Implementation()
+{
+	return false;
+}
+
+FReply UUR_Widget_BaseMenu::NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent)
+{
+	UE_LOG(LogTemp, Log, TEXT("OnKeyDown %s @ %s"), *InKeyEvent.GetKey().GetDisplayName().ToString(), *GetDisplayNameBase().ToString());
+
+	if (bCloseOnEscape && InKeyEvent.GetKey() == EKeys::Escape)
+	{
+		Close();
+	}
+	else if (CloseOnAction.Len() > 0 && UUR_FunctionLibrary::IsKeyMappedToAction(InKeyEvent.GetKey(), FName(*CloseOnAction)))
+	{
+		Close();
+	}
+	else
+	{
+		// fwd to UMG
+		Super::NativeOnKeyDown(InGeometry, InKeyEvent);
+	}
+
+	// capture all
+	return FReply::Handled();
+}
