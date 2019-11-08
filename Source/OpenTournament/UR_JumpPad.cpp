8272e46082f64f25cd10cf8ad19b5f50dc4becc3
wail <wailofsuicide@gmail.com>
2019-11-08 00:34:20 -0500

    Updating JumpPad - Code Cleanup & Tweaking
    - Add Project Info
    - Fix up Includes in .h and .cpp
    - Tabs to Spaces
    - Add Scene Root Component
    - Make Destination Vector into Transform with EditWidget
    - Add Editor functions to recalculate Destination Transform relative to Actor movement, rotation, etc.
    - Destination is relative to actor now since movable transform widget is relative to the actor

--- Source/OpenTournament/UR_JumpPad.cpp
+++ Source/OpenTournament/UR_JumpPad.cpp
@@ -1,77 +1,142 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
 
 #include "UR_JumpPad.h"
 
+#include "Engine/Engine.h"
+#include "Components/AudioComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+
+#include "OpenTournament.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+#include "AutomationTest.h"
+#endif
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Sets default values
 AUR_JumpPad::AUR_JumpPad(const FObjectInitializer& ObjectInitializer) :
-	Super(ObjectInitializer),
-	Destination(FVector(0, 0, 1000)),
-	JumpTime(2)
+    Super(ObjectInitializer),
+    Destination(FTransform()),
+    JumpTime(2.f),
+    JumpPadLaunchSound(nullptr)
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = false;
 
-	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
-	CapsuleComponent->SetCapsuleSize(45.f, 90.f, false);
-	SetRootComponent(CapsuleComponent);
-	CapsuleComponent->SetGenerateOverlapEvents(true);
-	CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &AUR_JumpPad::OnTriggerEnter);
+    SceneRoot = ObjectInitializer.CreateDefaultSubobject<USceneComponent>(this, TEXT("SceneComponent"));
+    RootComponent = SceneRoot;
 
-	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComponent"));
-	MeshComponent->SetupAttachment(RootComponent);
+    CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+    CapsuleComponent->SetCapsuleSize(45.f, 45.f, false);
+    CapsuleComponent->SetupAttachment(RootComponent);
+    
+    //SetRootComponent(CapsuleComponent);
+    CapsuleComponent->SetGenerateOverlapEvents(true);
+    CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &AUR_JumpPad::OnTriggerEnter);
 
-	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
-	AudioComponent->SetupAttachment(CapsuleComponent);
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComponent"));
+    MeshComponent->SetupAttachment(RootComponent);
 
-	ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystemComponent"));
-	ParticleSystemComponent->SetupAttachment(RootComponent);
+    AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+    AudioComponent->SetupAttachment(RootComponent);
 
-	Destination = GetActorLocation() + FVector::UpVector * 1000;
+    ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystemComponent"));
+    ParticleSystemComponent->SetupAttachment(RootComponent);
+
+    Destination = GetActorTransform();
+    Destination.SetLocation(Destination.GetLocation() + FVector(0, 0, 1000));
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Called when the game starts or when spawned
 void AUR_JumpPad::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 // Called every frame
 void AUR_JumpPad::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
 void AUR_JumpPad::OnTriggerEnter(UPrimitiveComponent* HitComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	const auto TargetCharacter{ Cast<ACharacter>(Other) };
+    const auto TargetCharacter{ Cast<ACharacter>(Other) };
 
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("[JUMPPAD] Entered jump pad %s"), *GetName()));
+    if (Other->GetActorLocation().Z > CapsuleComponent->GetComponentTransform().GetLocation().Z)
+    {
+        GAME_LOG(Game, Log, "Entered JumpPad (%s)", *GetName());
 
-	if (TargetCharacter)
-	{
-		TargetCharacter->LaunchCharacter(CalculateJumpVelocity(TargetCharacter), true, true);
-		PlayJumpPadEffects();
-	}
+        if (TargetCharacter)
+        {
+            TargetCharacter->LaunchCharacter(CalculateJumpVelocity(TargetCharacter), true, true);
+            PlayJumpPadEffects();
+        }
+    }
 }
 
 void AUR_JumpPad::PlayJumpPadEffects_Implementation()
 {
-	if (JumpPadLaunchSound)
-	{
-		UGameplayStatics::PlaySoundAtLocation(GetWorld(), JumpPadLaunchSound, this->GetActorLocation());
-	}
+    if (JumpPadLaunchSound)
+    {
+        UGameplayStatics::PlaySoundAtLocation(GetWorld(), JumpPadLaunchSound, GetActorLocation());
+    }
 }
 
-FVector AUR_JumpPad::CalculateJumpVelocity(AActor* Other)
+FVector AUR_JumpPad::CalculateJumpVelocity(const AActor* InCharacter)
 {
-	float gravity = GetWorld()->GetGravityZ();
-	FVector targetVector = Destination - Other->GetActorLocation();
+    float Gravity = GetWorld()->GetGravityZ();
+    FVector TargetVector = (GetActorLocation() + Destination.GetLocation()) - InCharacter->GetActorTransform().GetLocation();
 
-	float sizeXY = targetVector.Size2D() / JumpTime;
-	float sizeZ = targetVector.Z / JumpTime - gravity * JumpTime / 2.0f;
+    float SizeXY = TargetVector.Size2D() / JumpTime;
+    float SizeZ = TargetVector.Z / JumpTime - Gravity * JumpTime / 2.0f;
 
-	return targetVector.GetSafeNormal2D() * sizeXY + FVector::UpVector * sizeZ;
+    return TargetVector.GetSafeNormal2D() * SizeXY + FVector::UpVector * SizeZ;
 }
+
+#if WITH_EDITOR
+// Workarounds to the editor FTransform widget not having a setting to work in world space
+void AUR_JumpPad::EditorApplyTranslation(const FVector& DeltaTranslation, bool bAltDown, bool bShiftDown, bool bCtrlDown)
+{
+    FTransform CachedDestination = Destination * ActorToWorld();
+    Super::EditorApplyTranslation(DeltaTranslation, bAltDown, bShiftDown, bCtrlDown);
+
+    if (bLockDestination)
+    {
+        Destination = CachedDestination;
+        Destination.SetToRelativeTransform(ActorToWorld());
+    }
+}
+
+void AUR_JumpPad::EditorApplyRotation(const FRotator& DeltaRotation, bool bAltDown, bool bShiftDown, bool bCtrlDown)
+{
+    FTransform CachedDestination = Destination * ActorToWorld();
+    Super::EditorApplyRotation(DeltaRotation, bAltDown, bShiftDown, bCtrlDown);
+
+    if (bLockDestination)
+    {
+        Destination = CachedDestination;
+        Destination.SetToRelativeTransform(ActorToWorld());
+    }
+}
+
+void AUR_JumpPad::EditorApplyScale(const FVector& DeltaScale, const FVector* PivotLocation, bool bAltDown, bool bShiftDown, bool bCtrlDown)
+{
+    FTransform CachedDestination = Destination * ActorToWorld();
+    Super::EditorApplyScale(DeltaScale, PivotLocation, bAltDown, bShiftDown, bCtrlDown);
+
+    if (bLockDestination)
+    {
+        Destination = CachedDestination;
+        Destination.SetToRelativeTransform(ActorToWorld());
+    }
+}
+#endif
--- Source/OpenTournament/UR_JumpPad.h
+++ Source/OpenTournament/UR_JumpPad.h
@@ -1,15 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "Components/BoxComponent.h"
-#include "UR_Character.h"
 #include "UR_JumpPad.generated.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+class AActor;
 class UAudioComponent;
 class UCapsuleComponent;
 class USoundBase;
@@ -21,68 +18,86 @@ class UParticleSystemComponent;
 UCLASS()
 class OPENTOURNAMENT_API AUR_JumpPad : public AActor
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
-	AUR_JumpPad(const FObjectInitializer& ObjectInitializer);
+    // Sets default values for this actor's properties
+    AUR_JumpPad(const FObjectInitializer& ObjectInitializer);
 
-	/*
-	* Static Mesh Component - JumpPad Base
-	*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Jump Pad", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* MeshComponent;
+    UPROPERTY(BlueprintReadOnly, Category = JumpPad)
+    USceneComponent* SceneRoot;
 
-	/*
-	* Capsule Component - Active JumpPad Region
-	*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Jump Pad", meta = (AllowPrivateAccess = "true"))
-	UCapsuleComponent* CapsuleComponent;
-	
-	/*
-	* Audio Component
-	*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Teleporter", meta = (AllowPrivateAccess = "true"))
-	UAudioComponent* AudioComponent;
+    /*
+    * Static Mesh Component - JumpPad Base
+    */
+    UPROPERTY(BlueprintReadOnly, Category = "JumpPad", meta = (AllowPrivateAccess = "true"))
+    UStaticMeshComponent* MeshComponent;
 
-	/*
-	* ParticleSystem Component
-	*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Teleporter", meta = (AllowPrivateAccess = "true"))
-	UParticleSystemComponent* ParticleSystemComponent;
+    /*
+    * Capsule Component - Active JumpPad Region
+    */
+    UPROPERTY(BlueprintReadOnly, Category = "JumpPad", meta = (AllowPrivateAccess = "true"))
+    UCapsuleComponent* CapsuleComponent;
+    
+    /*
+    * Audio Component
+    */
+    UPROPERTY(BlueprintReadOnly, Category = "JumpPad", meta = (AllowPrivateAccess = "true"))
+    UAudioComponent* AudioComponent;
 
-	/*
-	* Destination of the
-	Jump - May be another JumpPad, TargetPoint, etc.
-	*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Jump Pad")
-	FVector Destination;
+    /*
+    * ParticleSystem Component
+    */
+    UPROPERTY(BlueprintReadOnly, Category = "JumpPad", meta = (AllowPrivateAccess = "true"))
+    UParticleSystemComponent* ParticleSystemComponent;
 
-	/*
-	* Duration of the Jump - in seconds
-	*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Jump Pad")
-	float JumpTime;
+    /////////////////////////////////////////////////////////////////////////////////////////////////
 
-	/**
-	* Actor is launched by JumpPad
-	*/
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Pad")
-	USoundBase* JumpPadLaunchSound;
+    /*
+    * Destination of the Jump - May be another JumpPad, TargetPoint, etc.
+    */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JumpPad", meta = (MakeEditWidget = ""))
+    FTransform Destination;
+
+#if WITH_EDITOR
+    /** if set then lock the Destination when moving/rotating the JumpPad */
+    UPROPERTY(EditAnywhere, Category = "JumpPadEditor")
+    bool bLockDestination;
+#endif
+
+    /*
+    * Duration of the Jump - in seconds
+    */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "JumpPad")
+    float JumpTime;
+
+    /**
+    * Actor is launched by JumpPad
+    */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JumpPad")
+    USoundBase* JumpPadLaunchSound;
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
 public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Jump Pad")
-	void PlayJumpPadEffects();
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "JumpPad")
+    void PlayJumpPadEffects();
 
-	FVector CalculateJumpVelocity(AActor* character);
+    FVector CalculateJumpVelocity(const AActor* InCharacter);
 
-	UFUNCTION()
-	void OnTriggerEnter(class UPrimitiveComponent* HitComp, class AActor* Other, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+    UFUNCTION()
+    void OnTriggerEnter(class UPrimitiveComponent* HitComp, class AActor* Other, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+#if WITH_EDITOR
+    virtual void EditorApplyTranslation(const FVector& DeltaTranslation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+    virtual void EditorApplyRotation(const FRotator& DeltaRotation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+    virtual void EditorApplyScale(const FVector& DeltaScale, const FVector* PivotLocation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+#endif
 };
