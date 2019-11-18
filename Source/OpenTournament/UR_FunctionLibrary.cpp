a56cbc961fd69f4b58925aa013c1322e6f1c9144
wail <wailofsuicide@gmail.com>
2019-11-17 21:58:16 -0500

    Add GetTimeString utility function
    - Plus tabs to spaces cleanup

--- Source/OpenTournament/UR_FunctionLibrary.cpp
+++ Source/OpenTournament/UR_FunctionLibrary.cpp
@@ -54,98 +54,109 @@ int32 UUR_FunctionLibrary::GetPlayerStateValue(APlayerController* PlayerControll
 
 FColor UUR_FunctionLibrary::GetPlayerDisplayTextColor(APlayerState* PS)
 {
-	if (!PS)
-	{
-		return FColorList::Green;
-	}
-	else if (PS->bOnlySpectator)
-	{
-		return GetSpectatorDisplayTextColor();
-	}
-	else
-	{
-		AUR_PlayerState* URPS = Cast<AUR_PlayerState>(PS);
-		if (URPS)
-		{
-			//TODO: if team game, return team color, something like URPS->Team->GetDisplayTextColor();
+    if (!PS)
+    {
+        return FColorList::Green;
+    }
+    else if (PS->bOnlySpectator)
+    {
+        return GetSpectatorDisplayTextColor();
+    }
+    else
+    {
+        AUR_PlayerState* URPS = Cast<AUR_PlayerState>(PS);
+        if (URPS)
+        {
+            //TODO: if team game, return team color, something like URPS->Team->GetDisplayTextColor();
 
-			//TODO: if non team game, return player's color ? if any ? or white ?
+            //TODO: if non team game, return player's color ? if any ? or white ?
 
-			return FColorList::Red;
-		}
-		else
-		{
-			return FColorList::Green;	//???
-		}
-	}
+            return FColorList::Red;
+        }
+        else
+        {
+            return FColorList::Green;	//???
+        }
+    }
 }
 
 FString UUR_FunctionLibrary::StripRichTextDecorators(const FString& InText)
 {
-	FString Result(TEXT(""), InText.Len());
+    FString Result(TEXT(""), InText.Len());
 
-	int32 CurrentPosition = 0;
+    int32 CurrentPosition = 0;
 
-	// see RichTextMarkupProcessing.cpp
+    // see RichTextMarkupProcessing.cpp
 
-	TArray<FTextRange> LineRanges;
-	FTextRange::CalculateLineRangesFromString(InText, LineRanges);
+    TArray<FTextRange> LineRanges;
+    FTextRange::CalculateLineRangesFromString(InText, LineRanges);
 
-	FRegexPattern ElementRegexPattern(TEXT("<([\\w\\d\\.-]+)((?: (?:[\\w\\d\\.-]+=(?>\".*?\")))+)?(?:(?:/>)|(?:>(.*?)</>))"));
-	FRegexMatcher ElementRegexMatcher(ElementRegexPattern, InText);
+    FRegexPattern ElementRegexPattern(TEXT("<([\\w\\d\\.-]+)((?: (?:[\\w\\d\\.-]+=(?>\".*?\")))+)?(?:(?:/>)|(?:>(.*?)</>))"));
+    FRegexMatcher ElementRegexMatcher(ElementRegexPattern, InText);
 
-	for (int32 i = 0; i < LineRanges.Num(); i++)
-	{
-		// Limit the element regex matcher to the current line
-		ElementRegexMatcher.SetLimits(LineRanges[i].BeginIndex, LineRanges[i].EndIndex);
+    for (int32 i = 0; i < LineRanges.Num(); i++)
+    {
+        // Limit the element regex matcher to the current line
+        ElementRegexMatcher.SetLimits(LineRanges[i].BeginIndex, LineRanges[i].EndIndex);
 
-		while (ElementRegexMatcher.FindNext())
-		{
-			// append all from current position up to opening marker
-			Result.Append(InText.Mid(CurrentPosition, ElementRegexMatcher.GetMatchBeginning() - CurrentPosition));
-			// append inner content
-			Result.Append(ElementRegexMatcher.GetCaptureGroup(3));
-			// set current position to after closing marker
-			CurrentPosition = ElementRegexMatcher.GetMatchEnding();
-		}
-	}
+        while (ElementRegexMatcher.FindNext())
+        {
+            // append all from current position up to opening marker
+            Result.Append(InText.Mid(CurrentPosition, ElementRegexMatcher.GetMatchBeginning() - CurrentPosition));
+            // append inner content
+            Result.Append(ElementRegexMatcher.GetCaptureGroup(3));
+            // set current position to after closing marker
+            CurrentPosition = ElementRegexMatcher.GetMatchEnding();
+        }
+    }
 
-	Result.Append(InText.Mid(CurrentPosition));
+    Result.Append(InText.Mid(CurrentPosition));
 
-	return Result;
+    return Result;
 }
 
 
 bool UUR_FunctionLibrary::IsKeyMappedToAction(const FKey& Key, FName ActionName)
 {
-	UInputSettings* Settings = UInputSettings::GetInputSettings();
-	TArray<FInputActionKeyMapping> Mappings;
-	Settings->GetActionMappingByName(ActionName, Mappings);
-	for (const auto& Mapping : Mappings)
-	{
-		if (Mapping.Key == Key)
-			return true;
-	}
-	return false;
+    UInputSettings* Settings = UInputSettings::GetInputSettings();
+    TArray<FInputActionKeyMapping> Mappings;
+    Settings->GetActionMappingByName(ActionName, Mappings);
+    for (const auto& Mapping : Mappings)
+    {
+        if (Mapping.Key == Key)
+            return true;
+    }
+    return false;
 }
 
 bool UUR_FunctionLibrary::IsKeyMappedToAxis(const FKey& Key, FName AxisName, float Direction)
 {
-	UInputSettings* Settings = UInputSettings::GetInputSettings();
-	TArray<FInputAxisKeyMapping> Mappings;
-	Settings->GetAxisMappingByName(AxisName, Mappings);
-	for (const auto& Mapping : Mappings)
-	{
-		if (Mapping.Key == Key && (FMath::IsNearlyZero(Direction) || (Direction*Mapping.Scale > 0)))
-			return true;
-	}
-	return false;
+    UInputSettings* Settings = UInputSettings::GetInputSettings();
+    TArray<FInputAxisKeyMapping> Mappings;
+    Settings->GetAxisMappingByName(AxisName, Mappings);
+    for (const auto& Mapping : Mappings)
+    {
+        if (Mapping.Key == Key && (FMath::IsNearlyZero(Direction) || (Direction*Mapping.Scale > 0)))
+            return true;
+    }
+    return false;
 }
 
 AUR_PlayerController* UUR_FunctionLibrary::GetLocalPlayerController(const UObject* WorldContextObject)
 {
-	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
-		return World->GetFirstPlayerController<AUR_PlayerController>();
+    if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+        return World->GetFirstPlayerController<AUR_PlayerController>();
 
-	return nullptr;
+    return nullptr;
+}
+
+FString UUR_FunctionLibrary::GetTimeString(const float TimeSeconds)
+{
+    // only minutes and seconds are relevant
+    const int32 TotalSeconds = FMath::Max(0, FMath::TruncToInt(TimeSeconds) % 3600);
+    const int32 NumMinutes = TotalSeconds / 60;
+    const int32 NumSeconds = TotalSeconds % 60;
+
+    const FString TimeDesc = FString::Printf(TEXT("%02d:%02d"), NumMinutes, NumSeconds);
+    return TimeDesc;
 }
