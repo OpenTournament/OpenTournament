43abb23cd2d56fd07f91d820bf4b1eeb1cbc6984
wail <wailofsuicide@gmail.com>
2019-12-14 12:13:44 -0500

    Enable GAS in Project

--- Source/OpenTournament/OpenTournament.Build.cs
+++ Source/OpenTournament/OpenTournament.Build.cs
@@ -8,6 +8,6 @@ public class OpenTournament : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
