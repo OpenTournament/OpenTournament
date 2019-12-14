2e12d3f1b918f18c8e3fe38ebc22598a7e1b3c61
wail <wailofsuicide@gmail.com>
2019-12-14 13:36:47 -0500

    Adding GAS AttributeSet

---
+++ Source/OpenTournament/UR_AttributeSet.cpp
@@ -0,0 +1,113 @@
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_AttributeSet.h"
+
+#include "UnrealNetwork.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+UUR_AttributeSet::UUR_AttributeSet()
+  : Health(1.f)
+  , HealthMax(1.f)
+  , Armor(0.0f)
+  , ArmorMax(1.0f)
+  , Shield(0.0f)
+  , ShieldMax(1.0f)
+  , Energy(0.f)
+  , EnergyMax(0.f)
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+void UUR_AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(UUR_AttributeSet, Health);
+    DOREPLIFETIME(UUR_AttributeSet, HealthMax);
+    DOREPLIFETIME(UUR_AttributeSet, Energy);
+    DOREPLIFETIME(UUR_AttributeSet, EnergyMax);
+    DOREPLIFETIME(UUR_AttributeSet, Armor);
+    DOREPLIFETIME(UUR_AttributeSet, ArmorMax);
+    DOREPLIFETIME(UUR_AttributeSet, Shield);
+    DOREPLIFETIME(UUR_AttributeSet, ShieldMax);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+void UUR_AttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+    // This is called whenever attributes change, so for max health (etc) we want to scale the current totals to match
+    Super::PreAttributeChange(Attribute, NewValue);
+
+    if (Attribute == GetHealthMaxAttribute())
+    {
+        AdjustAttributeForMaxChange(Health, HealthMax, NewValue, GetHealthAttribute());
+    }
+    else if (Attribute == GetEnergyMaxAttribute())
+    {
+        AdjustAttributeForMaxChange(Energy, EnergyMax, NewValue, GetEnergyAttribute());
+    }
+}
+
+void UUR_AttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+    Super::PostGameplayEffectExecute(Data);
+}
+
+void UUR_AttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty)
+{
+    UAbilitySystemComponent* AbilityComponent = GetOwningAbilitySystemComponent();
+    const float CurrentMaxValue = MaxAttribute.GetCurrentValue();
+    if (!FMath::IsNearlyEqual(CurrentMaxValue, NewMaxValue) && AbilityComponent)
+    {
+        // Change current value to maintain the current Val / Max percent
+        const float CurrentValue = AffectedAttribute.GetCurrentValue();
+        float NewDelta = (CurrentMaxValue > 0.f) ? (CurrentValue * NewMaxValue / CurrentMaxValue) - CurrentValue : NewMaxValue;
+
+        AbilityComponent->ApplyModToAttributeUnsafe(AffectedAttributeProperty, EGameplayModOp::Additive, NewDelta);
+    }
+}
+
+void UUR_AttributeSet::OnRep_Health()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, Health);
+}
+
+void UUR_AttributeSet::OnRep_HealthMax()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, HealthMax);
+}
+
+void UUR_AttributeSet::OnRep_Energy()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, Energy);
+}
+
+void UUR_AttributeSet::OnRep_EnergyMax()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, EnergyMax);
+}
+
+void UUR_AttributeSet::OnRep_Armor()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, Armor);
+}
+
+void UUR_AttributeSet::OnRep_ArmorMax()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, ArmorMax);
+}
+
+void UUR_AttributeSet::OnRep_Shield()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, Shield);
+}
+
+void UUR_AttributeSet::OnRep_ShieldMax()
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UUR_AttributeSet, ShieldMax);
+}
---
+++ Source/OpenTournament/UR_AttributeSet.h
@@ -0,0 +1,148 @@
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+
+#include "OpenTournament.h"
+#include "UR_AttributeSet.generated.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+    GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+* Character - Primary Attribute Set
+*/
+UCLASS()
+class OPENTOURNAMENT_API UUR_AttributeSet : public UAttributeSet
+{
+    GENERATED_BODY()
+
+public:
+
+    UUR_AttributeSet();
+    virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+    virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    UPROPERTY(ReplicatedUsing=OnRep_Health, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData Health;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, Health)
+
+    UPROPERTY(ReplicatedUsing=OnRep_HealthMax, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData HealthMax;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, HealthMax)
+
+    ////////========////////
+
+    UPROPERTY(ReplicatedUsing=OnRep_Armor, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData Armor;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, Armor)
+
+    UPROPERTY(ReplicatedUsing=OnRep_ArmorMax, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData ArmorMax;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, ArmorMax)
+
+    ////////========////////
+
+    UPROPERTY(ReplicatedUsing=OnRep_Shield, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData Shield;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, Shield)
+
+    UPROPERTY(ReplicatedUsing=OnRep_ShieldMax, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData ShieldMax;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, ShieldMax)
+
+    ////////========////////
+
+    UPROPERTY(ReplicatedUsing=OnRep_Energy, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData Energy;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, Energy)
+
+    UPROPERTY(ReplicatedUsing=OnRep_EnergyMax, BlueprintReadWrite, EditInstanceOnly, Category = "CharacterAttributes")
+    FGameplayAttributeData EnergyMax;
+    ATTRIBUTE_ACCESSORS(UUR_AttributeSet, EnergyMax)
+
+    ////////========////////
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData DamageScale;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData FireRate;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData ReloadRate;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData GroundSpeed;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData MovementAcceleration;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData CrouchedSpeed;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData SwimSpeed;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData JumpVelocity;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData AirControl;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData DodgeImpulseHorizontal;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CharacterAttributes")
+    FGameplayAttributeData DodgeImpulseVertical;
+
+    //-------------------------------------------------------------------------------------//
+
+protected:
+
+    /**
+    * Helper function to proportionally adjust the value of an attribute when it's associated max attribute changes.
+    * (i.e. When MaxHealth increases, Health increases by an amount that maintains the same percentage as before)
+    */
+    void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty);
+
+    // These OnRep functions exist to make sure that the ability system internal representations are synchronized properly during replication
+    UFUNCTION()
+    virtual void OnRep_Health();
+
+    UFUNCTION()
+    virtual void OnRep_HealthMax();
+
+    UFUNCTION()
+    virtual void OnRep_Energy();
+
+    UFUNCTION()
+    virtual void OnRep_EnergyMax();
+
+    UFUNCTION()
+    virtual void OnRep_Armor();
+
+    UFUNCTION()
+    virtual void OnRep_ArmorMax();
+
+    UFUNCTION()
+    virtual void OnRep_Shield();
+
+    UFUNCTION()
+    virtual void OnRep_ShieldMax();
+};
