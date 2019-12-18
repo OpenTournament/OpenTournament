22832d9dfcd45eafce41efa6a494abd46d563cdf
wail <wailofsuicide@gmail.com>
2019-12-17 23:07:41 -0500

    Adding core classes for GAS

---
+++ Source/OpenTournament/UR_AbilitySystemComponent.cpp
@@ -0,0 +1,52 @@
+// Copyright 2019 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_AbilitySystemComponent.h"
+
+//#include "Abilities/RPGGameplayAbility.h"
+#include "AbilitySystemGlobals.h"
+
+#include "UR_Character.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+UUR_AbilitySystemComponent::UUR_AbilitySystemComponent()
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+void UUR_AbilitySystemComponent::GetActiveAbilitiesWithTags(const FGameplayTagContainer& GameplayTagContainer, TArray<UGameplayAbility*>& ActiveAbilities)
+{
+    TArray<FGameplayAbilitySpec*> AbilitiesToActivate;
+    GetActivatableGameplayAbilitySpecsByAllMatchingTags(GameplayTagContainer, AbilitiesToActivate, false);
+
+    // Iterate the list of all ability specs
+    for (FGameplayAbilitySpec* Spec : AbilitiesToActivate)
+    {
+        // Iterate all instances on this ability spec
+        TArray<UGameplayAbility*> AbilityInstances = Spec->GetAbilityInstances();
+
+        for (UGameplayAbility* ActiveAbility : AbilityInstances)
+        {
+            ActiveAbilities.Add(Cast<UGameplayAbility>(ActiveAbility));
+        }
+    }
+}
+
+int32 UUR_AbilitySystemComponent::GetDefaultAbilityLevel() const
+{
+    AUR_Character* OwningCharacter = Cast<AUR_Character>(OwnerActor);
+
+    if (OwningCharacter)
+    {
+        //return OwningCharacter->GetCharacterLevel();
+    }
+    return 1;
+}
+
+UUR_AbilitySystemComponent* UUR_AbilitySystemComponent::GetAbilitySystemComponentFromActor(const AActor* Actor, bool LookForComponent)
+{
+    return Cast<UUR_AbilitySystemComponent>(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor, LookForComponent));
+}
