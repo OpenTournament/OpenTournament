3d75c379b90e295d3683bc07a4a5ccb9e3d2f6c9
wail <wailofsuicide@gmail.com>
2019-12-17 23:07:41 -0500

    Adding core classes for GAS

---
+++ Source/OpenTournament/UR_AbilityTypes.cpp
@@ -0,0 +1,37 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_AbilityTypes.h"
+
+#include "OpenTournament.h"
+#include "UR_AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool FUR_GameplayEffectContainerSpec::HasValidEffects() const
+{
+	return TargetGameplayEffectSpecs.Num() > 0;
+}
+
+bool FUR_GameplayEffectContainerSpec::HasValidTargets() const
+{
+	return TargetData.Num() > 0;
+}
+
+void FUR_GameplayEffectContainerSpec::AddTargets(const TArray<FHitResult>& HitResults, const TArray<AActor*>& TargetActors)
+{
+	for (const FHitResult& HitResult : HitResults)
+	{
+		FGameplayAbilityTargetData_SingleTargetHit* NewData = new FGameplayAbilityTargetData_SingleTargetHit(HitResult);
+		TargetData.Add(NewData);
+	}
+
+	if (TargetActors.Num() > 0)
+	{
+		FGameplayAbilityTargetData_ActorArray* NewData = new FGameplayAbilityTargetData_ActorArray();
+		NewData->TargetActorArray.Append(TargetActors);
+		TargetData.Add(NewData);
+	}
+}
