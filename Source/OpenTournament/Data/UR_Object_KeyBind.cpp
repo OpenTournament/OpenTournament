c94d9c062bfaa37d0df9f4381132572639db1454
wail <wailofsuicide@gmail.com>
2020-02-13 18:48:09 -0500

    Cleanup Object Keybind

--- Source/OpenTournament/Data/UR_Object_KeyBind.cpp
+++ Source/OpenTournament/Data/UR_Object_KeyBind.cpp
@@ -1,8 +1,11 @@
-// Copyright 2019-2020 Open Tournament Project, All Rights Reserved.
+// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "UR_Object_KeyBind.h"
 
-UUR_Object_KeyBind::UUR_Object_KeyBind()
+UUR_Object_KeyBind::UUR_Object_KeyBind() :
+    Name(""),
+    Key("")
 {
 }
