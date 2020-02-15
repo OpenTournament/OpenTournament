00b4be579c6a90a178856251dc95361071270593
wail <wailofsuicide@gmail.com>
2020-02-15 17:02:15 -0500

    Cleanup WeaponBase

--- Source/OpenTournament/UR_WeaponBase.h
+++ Source/OpenTournament/UR_WeaponBase.h
@@ -1,13 +1,23 @@
-// Copyright (c) 2019 Open Tournament Project, All Rights Reserved.
+// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UR_PickupBase.h"
+
 #include "UR_WeaponBase.generated.h"
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+class AUR_Weapon;
+class USkeletalMesh;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 /**
- * 
+ * Weapon Base
  */
 UCLASS()
 class OPENTOURNAMENT_API AUR_WeaponBase : public AUR_PickupBase
@@ -18,7 +28,7 @@ public:
 	AUR_WeaponBase();
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSubclassOf<class AUR_Weapon> WeaponClass;
+	TSubclassOf<AUR_Weapon> WeaponClass;
 
 protected:
 #if WITH_EDITOR
@@ -35,6 +45,6 @@ public:
 	* TODO: We should probably use static mesh instead of skeletal.
 	*/
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Weapon")
-	static class USkeletalMesh* GetMeshForWeapon(const TSubclassOf<class AUR_Weapon> InWeaponClass);
+	static USkeletalMesh* GetMeshForWeapon(const TSubclassOf<AUR_Weapon> InWeaponClass);
 
 };
