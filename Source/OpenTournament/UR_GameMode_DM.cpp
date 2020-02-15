ac2eebd688617b9697858b0507a8ee68f54de1de
wail <wailofsuicide@gmail.com>
2020-02-15 16:56:53 -0500

    Cleanup GameMode DM

--- Source/OpenTournament/UR_GameMode_DM.cpp
+++ Source/OpenTournament/UR_GameMode_DM.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Open Tournament Project, All Rights Reserved.
+// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
