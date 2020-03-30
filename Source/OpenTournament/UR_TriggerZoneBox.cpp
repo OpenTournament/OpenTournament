34b95094d1000cdab401d31d9393c81f419220e8
wail <wailofsuicide@gmail.com>
2020-03-30 00:22:43 -0400

    Adding TriggerZone Actor Class
    - Generic class intended for use with Domination Points, Capture the Hill volumes, and a variety of other purposes
    - Detects Enter & Exit of LD-specifiable Actor class
    - Events on Enter & Exit
    - Hooks for binding functions to Enter & Exit

---
+++ Source/OpenTournament/UR_TriggerZoneBox.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_TriggerZoneBox.h"
+
+#include "Components/BoxComponent.h"
+
+#include "OpenTournament.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+AUR_TriggerZoneBox::AUR_TriggerZoneBox(const FObjectInitializer& ObjectInitializer) :
+    Super(ObjectInitializer),
+    BoxComponent(nullptr)
+{
+    BoxComponent = Cast<UBoxComponent>(CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent")));
+    BoxComponent->SetBoxExtent(FVector{ 256.f, 256.f, 256.f });
+
+    ShapeComponent = BoxComponent;
+    ShapeComponent->SetCollisionProfileName(TEXT("OverlapOnlyPawn")); //UCollisionProfile::OverlapPawnOnly_Name
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//#if WITH_DEV_AUTOMATION_TESTS
+//
+//IMPLEMENT_SIMPLE_AUTOMATION_TEST(FOpenTournamentTriggerZoneBoxTest, "OpenTournament.Feature.Levels.LevelFeatures.Actor", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+//
+//bool FOpenTournamentTriggerZoneBoxTest::RunTest(const FString& Parameters)
+//{
+//    // TODO : Automated Tests
+//
+//    return true;
+//}
+//
+//#endif WITH_DEV_AUTOMATION_TESTS
