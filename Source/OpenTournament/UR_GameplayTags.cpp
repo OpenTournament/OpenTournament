0ec1191dded7118ed192484a5f4b336b899643f9
wail <wail-gaming@protonmail.com>
2023-02-10 01:00:51 -0500

    Initial Commit on UR_GameplayTags for NativeGameplayTags

---
+++ Source/OpenTournament/UR_GameplayTags.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2019-2023 Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_GameplayTags.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_MatchState_InProgress_Countdown ,    "MatchState.InProgress.Countdown");
+UE_DEFINE_GAMEPLAY_TAG(TAG_MatchState_InProgress_Match,         "MatchState.InProgress.Match");
+UE_DEFINE_GAMEPLAY_TAG(TAG_MatchState_InProgress_Overtime,      "MatchState.InProgress.Overtime");
+UE_DEFINE_GAMEPLAY_TAG(TAG_MatchState_InProgress_Warmup,        "MatchState.InProgress.Warmup");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Movement_Jumping,   "Character.States.Movement.Jumping");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Movement_Dodging,   "Character.States.Movement.Dodging");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Movement_Crouching, "Character.States.Movement.Crouching");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Movement_Running,   "Character.States.Movement.Running");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Physics_Walking,  "Character.States.Movement.Walking");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Physics_Falling,  "Character.States.Movement.Falling");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Physics_Swimming, "Character.States.Movement.Swimming");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_States_Physics_Flying,   "Character.States.Movement.Flying");
