211dd3eec674b358358dfd023655bb135baeb1f8
wail <wail-gaming@protonmail.com>
2023-12-16 22:17:10 -0500

    Add ExperienceManager, ExperienceDefinition Classes

---
+++ Source/OpenTournament/GameModes/UR_ExperienceManager.cpp
@@ -0,0 +1,56 @@
+// Copyright (c) Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_ExperienceManager.h"
+#include "GameModes/UR_ExperienceManager.h"
+#include "Engine/Engine.h"
+#include "Subsystems/SubsystemCollection.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(UR_ExperienceManager)
+
+#if WITH_EDITOR
+
+void UUR_ExperienceManager::OnPlayInEditorBegun()
+{
+	ensure(GameFeaturePluginRequestCountMap.IsEmpty());
+	GameFeaturePluginRequestCountMap.Empty();
+}
+
+void UUR_ExperienceManager::NotifyOfPluginActivation(const FString PluginURL)
+{
+	if (GIsEditor)
+	{
+		UUR_ExperienceManager* ExperienceManagerSubsystem = GEngine->GetEngineSubsystem<UUR_ExperienceManager>();
+		check(ExperienceManagerSubsystem);
+
+		// Track the number of requesters who activate this plugin. Multiple load/activation requests are always allowed because concurrent requests are handled.
+		int32& Count = ExperienceManagerSubsystem->GameFeaturePluginRequestCountMap.FindOrAdd(PluginURL);
+		++Count;
+	}
+}
+
+bool UUR_ExperienceManager::RequestToDeactivatePlugin(const FString PluginURL)
+{
+	if (GIsEditor)
+	{
+		UUR_ExperienceManager* ExperienceManagerSubsystem = GEngine->GetEngineSubsystem<UUR_ExperienceManager>();
+		check(ExperienceManagerSubsystem);
+
+		// Only let the last requester to get this far deactivate the plugin
+		int32& Count = ExperienceManagerSubsystem->GameFeaturePluginRequestCountMap.FindChecked(PluginURL);
+		--Count;
+
+		if (Count == 0)
+		{
+			ExperienceManagerSubsystem->GameFeaturePluginRequestCountMap.Remove(PluginURL);
+			return true;
+		}
+
+		return false;
+	}
+
+	return true;
+}
+
+#endif
