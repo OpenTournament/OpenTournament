2430b759b4b9cc7d45f1d22f46c151e2b8bcb97b
wail <wail-gaming@protonmail.com>
2023-12-17 12:32:07 -0500

    Adding PawnData DataAsset Stub

---
+++ Source/OpenTournament/Character/UR_PawnData.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "UR_PawnData.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(UR_PawnData)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+UUR_PawnData::UUR_PawnData(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PawnClass = nullptr;
+	//InputConfig = nullptr;
+	//DefaultCameraMode = nullptr;
+}
+
