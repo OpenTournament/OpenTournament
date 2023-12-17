0aade5cf275dc142ec9e523aed2796c5fcd2d70e
wail <wail-gaming@protonmail.com>
2023-12-16 19:50:42 -0500

    Add GameplayMessageProcessor class

---
+++ Source/OpenTournament/Messages/GameplayMessageProcessor.cpp
@@ -0,0 +1,59 @@
+// Copyright (c) Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "GameplayMessageProcessor.h"
+
+#include "Engine/World.h"
+#include "GameFramework/GameStateBase.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(GameplayMessageProcessor)
+
+void UGameplayMessageProcessor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartListening();
+}
+
+void UGameplayMessageProcessor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	StopListening();
+
+	// Remove any listener handles
+	UGameplayMessageSubsystem& MessageSubsystem = UGameplayMessageSubsystem::Get(this);
+	for (FGameplayMessageListenerHandle& Handle : ListenerHandles)
+	{
+		MessageSubsystem.UnregisterListener(Handle);
+	}
+	ListenerHandles.Empty();
+}
+
+void UGameplayMessageProcessor::StartListening()
+{
+
+}
+
+void UGameplayMessageProcessor::StopListening()
+{
+}
+
+void UGameplayMessageProcessor::AddListenerHandle(FGameplayMessageListenerHandle&& Handle)
+{
+	ListenerHandles.Add(MoveTemp(Handle));
+}
+
+double UGameplayMessageProcessor::GetServerTime() const
+{
+	if (const AGameStateBase* GameState = GetWorld()->GetGameState())
+	{
+		return GameState->GetServerWorldTimeSeconds();
+	}
+	else
+	{
+		return 0.0;
+	}
+}
+
