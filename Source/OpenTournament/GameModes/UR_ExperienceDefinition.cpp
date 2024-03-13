8e397ec43cca401a3620cfcb02527022397888cf
wail <wail-gaming@protonmail.com>
2024-03-12 23:13:19 -0400

    Formatting Pass on UR_ExperienceDefinition

--- Source/OpenTournament/GameModes/UR_ExperienceDefinition.cpp
+++ Source/OpenTournament/GameModes/UR_ExperienceDefinition.cpp
@@ -20,65 +20,65 @@ UUR_ExperienceDefinition::UUR_ExperienceDefinition()
 #if WITH_EDITOR
 EDataValidationResult UUR_ExperienceDefinition::IsDataValid(FDataValidationContext& Context) const
 {
-	EDataValidationResult Result = CombineDataValidationResults(Super::IsDataValid(Context), EDataValidationResult::Valid);
+    EDataValidationResult Result = CombineDataValidationResults(Super::IsDataValid(Context), EDataValidationResult::Valid);
 
-	int32 EntryIndex = 0;
-	for (const UGameFeatureAction* Action : Actions)
-	{
-		if (Action)
-		{
-			EDataValidationResult ChildResult = Action->IsDataValid(Context);
-			Result = CombineDataValidationResults(Result, ChildResult);
-		}
-		else
-		{
-			Result = EDataValidationResult::Invalid;
-			Context.AddError(FText::Format(LOCTEXT("ActionEntryIsNull", "Null entry at index {0} in Actions"), FText::AsNumber(EntryIndex)));
-		}
+    int32 EntryIndex = 0;
+    for (const UGameFeatureAction* Action : Actions)
+    {
+        if (Action)
+        {
+            EDataValidationResult ChildResult = Action->IsDataValid(Context);
+            Result = CombineDataValidationResults(Result, ChildResult);
+        }
+        else
+        {
+            Result = EDataValidationResult::Invalid;
+            Context.AddError(FText::Format(LOCTEXT("ActionEntryIsNull", "Null entry at index {0} in Actions"), FText::AsNumber(EntryIndex)));
+        }
 
-		++EntryIndex;
-	}
+        ++EntryIndex;
+    }
 
-	// Make sure users didn't subclass from a BP of this (it's fine and expected to subclass once in BP, just not twice)
-	if (!GetClass()->IsNative())
-	{
-		const UClass* ParentClass = GetClass()->GetSuperClass();
+    // Make sure users didn't subclass from a BP of this (it's fine and expected to subclass once in BP, just not twice)
+    if (!GetClass()->IsNative())
+    {
+        const UClass* ParentClass = GetClass()->GetSuperClass();
 
-		// Find the native parent
-		const UClass* FirstNativeParent = ParentClass;
-		while ((FirstNativeParent != nullptr) && !FirstNativeParent->IsNative())
-		{
-			FirstNativeParent = FirstNativeParent->GetSuperClass();
-		}
+        // Find the native parent
+        const UClass* FirstNativeParent = ParentClass;
+        while ((FirstNativeParent != nullptr) && !FirstNativeParent->IsNative())
+        {
+            FirstNativeParent = FirstNativeParent->GetSuperClass();
+        }
 
-		if (FirstNativeParent != ParentClass)
-		{
-			Context.AddError(FText::Format(LOCTEXT("ExperienceInheritenceIsUnsupported", "Blueprint subclasses of Blueprint experiences is not currently supported (use composition via ActionSets instead). Parent class was {0} but should be {1}."),
-				FText::AsCultureInvariant(GetPathNameSafe(ParentClass)),
-				FText::AsCultureInvariant(GetPathNameSafe(FirstNativeParent))
-			));
-			Result = EDataValidationResult::Invalid;
-		}
-	}
+        if (FirstNativeParent != ParentClass)
+        {
+            Context.AddError(FText::Format(LOCTEXT("ExperienceInheritenceIsUnsupported",
+                    "Blueprint subclasses of Blueprint experiences is not currently supported (use composition via ActionSets instead). Parent class was {0} but should be {1}."),
+                FText::AsCultureInvariant(GetPathNameSafe(ParentClass)),
+                FText::AsCultureInvariant(GetPathNameSafe(FirstNativeParent))
+            ));
+            Result = EDataValidationResult::Invalid;
+        }
+    }
 
-	return Result;
+    return Result;
 }
 #endif
 
 #if WITH_EDITORONLY_DATA
 void UUR_ExperienceDefinition::UpdateAssetBundleData()
 {
-	Super::UpdateAssetBundleData();
+    Super::UpdateAssetBundleData();
 
-	for (UGameFeatureAction* Action : Actions)
-	{
-		if (Action)
-		{
-			Action->AddAdditionalAssetBundleData(AssetBundleData);
-		}
-	}
+    for (UGameFeatureAction* Action : Actions)
+    {
+        if (Action)
+        {
+            Action->AddAdditionalAssetBundleData(AssetBundleData);
+        }
+    }
 }
 #endif // WITH_EDITORONLY_DATA
 
 #undef LOCTEXT_NAMESPACE
-
--- Source/OpenTournament/GameModes/UR_ExperienceDefinition.h
+++ Source/OpenTournament/GameModes/UR_ExperienceDefinition.h
@@ -7,8 +7,12 @@
 #include "Engine/DataAsset.h"
 #include "UR_ExperienceDefinition.generated.h"
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 class UGameFeatureAction;
 class UUR_ExperienceActionSet;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
  * Definition of an experience
@@ -16,35 +20,39 @@ class UUR_ExperienceActionSet;
 UCLASS(BlueprintType, Const)
 class UUR_ExperienceDefinition : public UPrimaryDataAsset
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	UUR_ExperienceDefinition();
+    UUR_ExperienceDefinition();
 
-	//~UObject interface
+    //~UObject interface
 #if WITH_EDITOR
-	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+    virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
 #endif
-	//~End of UObject interface
+    //~End of UObject interface
 
-	//~UPrimaryDataAsset interface
+    //~UPrimaryDataAsset interface
 #if WITH_EDITORONLY_DATA
-	virtual void UpdateAssetBundleData() override;
+    virtual void UpdateAssetBundleData() override;
 #endif
-	//~End of UPrimaryDataAsset interface
+    //~End of UPrimaryDataAsset interface
 
 public:
-	// List of Game Feature Plugins this experience wants to have active
-	UPROPERTY(EditDefaultsOnly, Category = Gameplay)
-	TArray<FString> GameFeaturesToEnable;
+    // List of Game Feature Plugins this experience wants to have active
+    UPROPERTY(EditDefaultsOnly, Category = Gameplay)
+    TArray<FString> GameFeaturesToEnable;
 
-	// List of actions to perform as this experience is loaded/activated/deactivated/unloaded
-	UPROPERTY(EditDefaultsOnly, Instanced, Category="Actions")
-	TArray<TObjectPtr<UGameFeatureAction>> Actions;
+    // List of actions to perform as this experience is loaded/activated/deactivated/unloaded
+    UPROPERTY(EditDefaultsOnly, Instanced, Category="Actions")
+    TArray<TObjectPtr<UGameFeatureAction>> Actions;
 
     // @! TODO PawnData
+    /** The default pawn class to spawn for players */
+    //@TODO: Make soft?
+    //UPROPERTY(EditDefaultsOnly, Category=Gameplay)
+    //TObjectPtr<const ULyraPawnData> DefaultPawnData;
 
-	// List of additional action sets to compose into this experience
-	UPROPERTY(EditDefaultsOnly, Category=Gameplay)
-	TArray<TObjectPtr<UUR_ExperienceActionSet>> ActionSets;
+    // List of additional action sets to compose into this experience
+    UPROPERTY(EditDefaultsOnly, Category=Gameplay)
+    TArray<TObjectPtr<UUR_ExperienceActionSet>> ActionSets;
 };
