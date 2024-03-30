823f2f68bb4389875bc443ef08c50775660ee566
wail <604328+wail@users.noreply.github.com>
2024-03-30 19:51:51 -0400

    Update HealthBase for CI (#321)

--- Source/OpenTournament/UR_HealthBase.cpp
+++ Source/OpenTournament/UR_HealthBase.cpp
@@ -1,10 +1,13 @@
-// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
+// Copyright (c) Open Tournament Project, All Rights Reserved.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "UR_HealthBase.h"
+
+#include "UR_AbilitySystemComponent.h"
 #include "UR_Character.h"
 #include "UR_AttributeSet.h"
+#include "UR_LogChannels.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -26,7 +29,7 @@ bool AUR_HealthBase::AllowPickupBy_Implementation(class AActor* Other)
 
         // Can only pick ups health packs if we are below 100
         AUR_Character* Char = Cast<AUR_Character>(Other);
-        return (Char && Char->AttributeSet && Char->AttributeSet->GetHealth() < Char->AttributeSet->GetHealthMax());
+        return (Char && Char->AttributeSet && Char->AttributeSet->GetHealth_D() < Char->AttributeSet->GetHealthMax_D());
     }
     return false;
 }
@@ -36,14 +39,25 @@ void AUR_HealthBase::GiveTo_Implementation(class AActor* Other)
     AUR_Character* Char = Cast<AUR_Character>(Other);
     if (Char && Char->AttributeSet)
     {
-        // @! TODO : This is Temporary. Healing should be done via GameplayEffect.
-        const int32 CurrentHealth = static_cast<int32>(Char->AttributeSet->GetHealth());
-        const int32 FinalHealth = FMath::Min<int32>(CurrentHealth + HealAmount, Char->AttributeSet->GetHealthMax() + (bSuperHeal ? Char->AttributeSet->GetOverHealthMax() : 0));
-        if (FinalHealth > CurrentHealth)
+
+        if (auto ASC = Char->GetGameAbilitySystemComponent())
         {
-            GAME_LOG(Game, Log, "Health Pickup: %d + %d -> %d", CurrentHealth, FinalHealth - CurrentHealth, FinalHealth);
-            Char->AttributeSet->SetHealth(FinalHealth);
+            if (EffectClass)
+            {
+                // @! TODO Make EffectSpec
+                //auto Effect = NewObject<EffectClass>(Char, EffectClass);
+                //ASC->ApplyGameplayEffectToSelf(Effect);
+            }
         }
+
+        // // @! TODO : This is Temporary. Healing should be done via GameplayEffect.
+        // const int32 CurrentHealth = static_cast<int32>(Char->AttributeSet->GetHealth_D());
+        // const int32 FinalHealth = FMath::Min<int32>(CurrentHealth + HealAmount, Char->AttributeSet->GetHealthMax_D() + (bSuperHeal ? Char->AttributeSet->GetOverHealthMax() : 0));
+        // if (FinalHealth > CurrentHealth)
+        // {
+        //     GAME_LOG(LogGame, Log, "Health Pickup: %d + %d -> %d", CurrentHealth, FinalHealth - CurrentHealth, FinalHealth);
+        //     Char->AttributeSet->SetHealth_D(FinalHealth);
+        // }
     }
 
     Super::GiveTo_Implementation(Other);
