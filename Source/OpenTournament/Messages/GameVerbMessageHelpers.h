88cbc2454024b70b50da7ccc2f876fd4b566ee2a
wail <604328+wail@users.noreply.github.com>
2024-04-04 20:02:44 -0400

    Cleanup GameVerbMessagingHelpers

--- Source/OpenTournament/Messages/GameVerbMessageHelpers.h
+++ Source/OpenTournament/Messages/GameVerbMessageHelpers.h
@@ -8,6 +8,8 @@
 
 #include "GameVerbMessageHelpers.generated.h"
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 struct FGameplayCueParameters;
 struct FGameVerbMessage;
 
@@ -16,22 +18,23 @@ class APlayerState;
 class UObject;
 struct FFrame;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 UCLASS()
 class OPENTOURNAMENT_API UGameVerbMessageHelpers : public UBlueprintFunctionLibrary
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = "OT")
-	static APlayerState* GetPlayerStateFromObject(UObject* Object);
+    UFUNCTION(BlueprintCallable, Category = "OT")
+    static APlayerState* GetPlayerStateFromObject(UObject* Object);
 
-	UFUNCTION(BlueprintCallable, Category = "OT")
-	static APlayerController* GetPlayerControllerFromObject(UObject* Object);
+    UFUNCTION(BlueprintCallable, Category = "OT")
+    static APlayerController* GetPlayerControllerFromObject(UObject* Object);
 
-	UFUNCTION(BlueprintCallable, Category = "OT")
-	static FGameplayCueParameters VerbMessageToCueParameters(const FGameVerbMessage& Message);
+    UFUNCTION(BlueprintCallable, Category = "OT")
+    static FGameplayCueParameters VerbMessageToCueParameters(const FGameVerbMessage& Message);
 
-	UFUNCTION(BlueprintCallable, Category = "OT")
-	static FGameVerbMessage CueParametersToVerbMessage(const FGameplayCueParameters& Params);
+    UFUNCTION(BlueprintCallable, Category = "OT")
+    static FGameVerbMessage CueParametersToVerbMessage(const FGameplayCueParameters& Params);
 };
