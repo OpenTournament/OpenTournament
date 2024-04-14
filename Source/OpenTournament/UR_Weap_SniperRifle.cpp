6e76ce3764bcb666d2e2bb219a0472635fbd44ff
wail <604328+wail@users.noreply.github.com>
2024-04-14 12:48:32 -0400

    Cleanup Weapon Subclasses
    - Reformatting, remove redundant classes, etc

--- Source/OpenTournament/UR_Weap_SniperRifle.cpp
+++ Source/OpenTournament/UR_Weap_SniperRifle.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
+// Copyright (c) Open Tournament Project, All Rights Reserved.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -9,13 +9,5 @@
 AUR_Weap_SniperRifle::AUR_Weap_SniperRifle(const FObjectInitializer& ObjectInitializer)
     : Super(ObjectInitializer)
 {
-    /*ConstructorHelpers::FObjectFinder<USkeletalMesh> newAsset(TEXT("SkeletalMesh'/Game/SciFiWeapDark/Weapons/Darkness_SniperRifle.Darkness_SniperRifle'"));
-    USkeletalMesh* helper = newAsset.Object;
-    Mesh1P->SetSkeletalMesh(helper);*/
     WeaponName = "Sniper Rifle";
-
-    /*ConstructorHelpers::FObjectFinder<USoundCue> newAssetSound(TEXT("SoundCue'/Game/SciFiWeapDark/Sound/SniperRifle/SniperRifle_Lower_Cue.SniperRifle_Lower_Cue'"));
-    USoundCue* helperSound;
-    helperSound = newAssetSound.Object;
-    Sound->SetSound(helperSound);*/
 }
