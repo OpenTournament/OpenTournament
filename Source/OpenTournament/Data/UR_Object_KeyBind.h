158fb84079ba11d0fc4a3854171cb23f40e3b1e4
wail <604328+wail@users.noreply.github.com>
2024-04-19 21:43:00 -0400

    Cleanup UR_Object_Keybind

--- Source/OpenTournament/Data/UR_Object_KeyBind.h
+++ Source/OpenTournament/Data/UR_Object_KeyBind.h
@@ -1,24 +1,24 @@
-// Copyright (c) 2019-2020 Open Tournament Project, All Rights Reserved.
+// Copyright (c) Open Tournament Project, All Rights Reserved.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "InputCoreTypes.h"
+
 #include "UR_Object_KeyBind.generated.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
- * 
+ *
  */
 UCLASS()
 class OPENTOURNAMENT_API UUR_Object_KeyBind : public UObject
 {
     GENERATED_BODY()
-    
+
 public:
     UUR_Object_KeyBind();
 
