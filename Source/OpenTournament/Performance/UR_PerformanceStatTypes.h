25ec6507e0f11be9a593e545bf0239defe749d6f
wail <604328+wail@users.noreply.github.com>
2024-09-07 21:53:43 -0400

    Add UR_PerformanceStatTypes

---
+++ Source/OpenTournament/Performance/UR_PerformanceStatTypes.h
@@ -0,0 +1,88 @@
+// Copyright (c) Open Tournament Project, All Rights Reserved.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/EnumRange.h"
+
+#include "UR_PerformanceStatTypes.generated.h"
+
+//////////////////////////////////////////////////////////////////////
+
+// Way to display the stat
+UENUM(BlueprintType)
+enum class EGameStatDisplayMode : uint8
+{
+    // Don't show this stat
+    Hidden,
+
+    // Show this stat in text form
+    TextOnly,
+
+    // Show this stat in graph form
+    GraphOnly,
+
+    // Show this stat as both text and graph
+    TextAndGraph
+};
+
+//////////////////////////////////////////////////////////////////////
+
+// Different kinds of stats that can be displayed on-screen
+UENUM(BlueprintType)
+enum class EGameDisplayablePerformanceStat : uint8
+{
+    // stat fps (in Hz)
+    ClientFPS,
+
+    // server tick rate (in Hz)
+    ServerFPS,
+
+    // idle time spent waiting for vsync or frame rate limit (in seconds)
+    IdleTime,
+
+    // Stat unit overall (in seconds)
+    FrameTime,
+
+    // Stat unit (game thread, in seconds)
+    FrameTime_GameThread,
+
+    // Stat unit (render thread, in seconds)
+    FrameTime_RenderThread,
+
+    // Stat unit (RHI thread, in seconds)
+    FrameTime_RHIThread,
+
+    // Stat unit (inferred GPU time, in seconds)
+    FrameTime_GPU,
+
+    // Network ping (in ms)
+    Ping,
+
+    // The incoming packet loss percentage (%)
+    PacketLoss_Incoming,
+
+    // The outgoing packet loss percentage (%)
+    PacketLoss_Outgoing,
+
+    // The number of packets received in the last second
+    PacketRate_Incoming,
+
+    // The number of packets sent in the past second
+    PacketRate_Outgoing,
+
+    // The avg. size (in bytes) of packets received
+    PacketSize_Incoming,
+
+    // The avg. size (in bytes) of packets sent
+    PacketSize_Outgoing,
+
+    // New stats should go above here
+    Count UMETA(Hidden)
+};
+
+ENUM_RANGE_BY_COUNT(EGameDisplayablePerformanceStat, EGameDisplayablePerformanceStat::Count);
+
+//////////////////////////////////////////////////////////////////////
