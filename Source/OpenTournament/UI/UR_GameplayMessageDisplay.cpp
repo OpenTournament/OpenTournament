70e57f93d41ebfd38bfd001551fce49f508eebc8
raqsi <raksi@purelymail.com>
2024-09-23 00:11:57 +0200

    Setup UI default experience and policy for lyra style UI

---
+++ Source/OpenTournament/UI/UR_GameplayMessageDisplay.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Open Tournament Project, All Rights Reserved.
+
+
+#include "UR_GameplayMessageDisplay.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+
+void UUR_GameplayMessageDisplay::NativeOnActivated()
+{
+    Super::NativeOnActivated();
+
+    UGameplayMessageSubsystem& MessageSystem = UGameplayMessageSubsystem::Get(GetWorld());
+
+    for(const auto Tag : GameplayMessagesToListenFor)
+    {
+        ListenerHandles.Add(MessageSystem.RegisterListener(Tag, this, &UUR_GameplayMessageDisplay::MessageReceived));
+    }
+}
+
+void UUR_GameplayMessageDisplay::NativeOnDeactivated()
+{
+    Super::NativeOnDeactivated();
+    UGameplayMessageSubsystem& MessageSystem = UGameplayMessageSubsystem::Get(GetWorld());
+
+    for(const auto ListenerHandle : ListenerHandles)
+        MessageSystem.UnregisterListener(ListenerHandle);
+}
---
+++ Source/OpenTournament/UI/UR_GameplayMessageDisplay.h
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Open Tournament Project, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameVerbMessage.h"
+#include "UR_ActivatableWidget.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "UR_GameplayMessageDisplay.generated.h"
+
+
+UCLASS(Blueprintable)
+class OPENTOURNAMENT_API UUR_GameplayMessageDisplay : public UUR_ActivatableWidget
+{
+    GENERATED_BODY()
+
+protected:
+
+    UPROPERTY(EditDefaultsOnly)
+    FGameplayTagContainer GameplayMessagesToListenFor;
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void MessageReceived(FGameplayTag Tag, const FGameVerbMessage& Message);
+
+    virtual void NativeOnActivated() override;
+    virtual void NativeOnDeactivated() override;
+
+private:
+
+    TArray<FGameplayMessageListenerHandle> ListenerHandles;
+};
