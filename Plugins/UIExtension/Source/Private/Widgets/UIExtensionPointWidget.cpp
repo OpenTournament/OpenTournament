008986c10a607df8b6f5ce3ef69e24afc4c6a25b
wail <604328+wail@users.noreply.github.com>
2024-12-18 16:33:10 -0500

    Cleanup UIExtension Classes + Coding Style Rules

--- Plugins/UIExtension/Source/Private/Widgets/UIExtensionPointWidget.cpp
+++ Plugins/UIExtension/Source/Private/Widgets/UIExtensionPointWidget.cpp
@@ -16,162 +16,173 @@
 // UUIExtensionPointWidget
 
 UUIExtensionPointWidget::UUIExtensionPointWidget(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
+    : Super(ObjectInitializer)
 {
-
 }
 
 void UUIExtensionPointWidget::ReleaseSlateResources(bool bReleaseChildren)
 {
-	ResetExtensionPoint();
+    ResetExtensionPoint();
 
-	Super::ReleaseSlateResources(bReleaseChildren);
+    Super::ReleaseSlateResources(bReleaseChildren);
 }
 
 TSharedRef<SWidget> UUIExtensionPointWidget::RebuildWidget()
 {
-	if (!IsDesignTime() && ExtensionPointTag.IsValid())
-	{
-		ResetExtensionPoint();
-		RegisterExtensionPoint();
+    if (!IsDesignTime() && ExtensionPointTag.IsValid())
+    {
+        ResetExtensionPoint();
+        RegisterExtensionPoint();
 
-		FDelegateHandle Handle = GetOwningLocalPlayer<UCommonLocalPlayer>()->CallAndRegister_OnPlayerStateSet(
-			UCommonLocalPlayer::FPlayerStateSetDelegate::FDelegate::CreateUObject(this, &UUIExtensionPointWidget::RegisterExtensionPointForPlayerState)
-		);
-	}
+        FDelegateHandle Handle = GetOwningLocalPlayer<UCommonLocalPlayer>()->CallAndRegister_OnPlayerStateSet
+        (
+            UCommonLocalPlayer::FPlayerStateSetDelegate::FDelegate::CreateUObject(this, &UUIExtensionPointWidget::RegisterExtensionPointForPlayerState)
+        );
+    }
 
-	if (IsDesignTime())
-	{
-		auto GetExtensionPointText = [this]()
-		{
-			return FText::Format(LOCTEXT("DesignTime_ExtensionPointLabel", "Extension Point\n{0}"), FText::FromName(ExtensionPointTag.GetTagName()));
-		};
+    if (IsDesignTime())
+    {
+        auto GetExtensionPointText = [this]()
+        {
+            return FText::Format(LOCTEXT("DesignTime_ExtensionPointLabel", "Extension Point\n{0}"), FText::FromName(ExtensionPointTag.GetTagName()));
+        };
 
-		TSharedRef<SOverlay> MessageBox = SNew(SOverlay);
+        TSharedRef<SOverlay> MessageBox = SNew(SOverlay);
 
-		MessageBox->AddSlot()
-			.Padding(5.0f)
-			.HAlign(HAlign_Center)
-			.VAlign(VAlign_Center)
-			[
-				SNew(STextBlock)
-				.Justification(ETextJustify::Center)
-				.Text_Lambda(GetExtensionPointText)
-			];
+        MessageBox->AddSlot()
+                  .Padding(5.0f)
+                  .HAlign(HAlign_Center)
+                  .VAlign(VAlign_Center)
+        [
+            SNew(STextBlock)
+            .Justification(ETextJustify::Center)
+            .Text_Lambda(GetExtensionPointText)
+        ];
 
-		return MessageBox;
-	}
-	else
-	{
-		return Super::RebuildWidget();
-	}
+        return MessageBox;
+    }
+    else
+    {
+        return Super::RebuildWidget();
+    }
 }
 
 void UUIExtensionPointWidget::ResetExtensionPoint()
 {
-	ResetInternal();
+    ResetInternal();
 
-	ExtensionMapping.Reset();
-	for (FUIExtensionPointHandle& Handle : ExtensionPointHandles)
-	{
-		Handle.Unregister();
-	}
-	ExtensionPointHandles.Reset();
+    ExtensionMapping.Reset();
+    for (FUIExtensionPointHandle& Handle : ExtensionPointHandles)
+    {
+        Handle.Unregister();
+    }
+    ExtensionPointHandles.Reset();
 }
 
 void UUIExtensionPointWidget::RegisterExtensionPoint()
 {
-	if (UUIExtensionSubsystem* ExtensionSubsystem = GetWorld()->GetSubsystem<UUIExtensionSubsystem>())
-	{
-		TArray<UClass*> AllowedDataClasses;
-		AllowedDataClasses.Add(UUserWidget::StaticClass());
-		AllowedDataClasses.Append(DataClasses);
+    if (UUIExtensionSubsystem* ExtensionSubsystem = GetWorld()->GetSubsystem<UUIExtensionSubsystem>())
+    {
+        TArray<UClass*> AllowedDataClasses;
+        AllowedDataClasses.Add(UUserWidget::StaticClass());
+        AllowedDataClasses.Append(DataClasses);
 
-		ExtensionPointHandles.Add(ExtensionSubsystem->RegisterExtensionPoint(
-			ExtensionPointTag, ExtensionPointTagMatch, AllowedDataClasses,
-			FExtendExtensionPointDelegate::CreateUObject(this, &ThisClass::OnAddOrRemoveExtension)
-		));
+        ExtensionPointHandles.Add
+        (ExtensionSubsystem->RegisterExtensionPoint
+            (
+                ExtensionPointTag,
+                ExtensionPointTagMatch,
+                AllowedDataClasses,
+                FExtendExtensionPointDelegate::CreateUObject(this, &ThisClass::OnAddOrRemoveExtension)
+            ));
 
-		ExtensionPointHandles.Add(ExtensionSubsystem->RegisterExtensionPointForContext(
-			ExtensionPointTag, GetOwningLocalPlayer(), ExtensionPointTagMatch, AllowedDataClasses,
-			FExtendExtensionPointDelegate::CreateUObject(this, &ThisClass::OnAddOrRemoveExtension)
-		));
-	}
+        ExtensionPointHandles.Add
+        (ExtensionSubsystem->RegisterExtensionPointForContext
+            (
+                ExtensionPointTag,
+                GetOwningLocalPlayer(),
+                ExtensionPointTagMatch,
+                AllowedDataClasses,
+                FExtendExtensionPointDelegate::CreateUObject(this, &ThisClass::OnAddOrRemoveExtension)
+            ));
+    }
 }
 
 void UUIExtensionPointWidget::RegisterExtensionPointForPlayerState(UCommonLocalPlayer* LocalPlayer, APlayerState* PlayerState)
 {
-	if (UUIExtensionSubsystem* ExtensionSubsystem = GetWorld()->GetSubsystem<UUIExtensionSubsystem>())
-	{
-		TArray<UClass*> AllowedDataClasses;
-		AllowedDataClasses.Add(UUserWidget::StaticClass());
-		AllowedDataClasses.Append(DataClasses);
+    if (UUIExtensionSubsystem* ExtensionSubsystem = GetWorld()->GetSubsystem<UUIExtensionSubsystem>())
+    {
+        TArray<UClass*> AllowedDataClasses;
+        AllowedDataClasses.Add(UUserWidget::StaticClass());
+        AllowedDataClasses.Append(DataClasses);
 
-		ExtensionPointHandles.Add(ExtensionSubsystem->RegisterExtensionPointForContext(
-			ExtensionPointTag, PlayerState, ExtensionPointTagMatch, AllowedDataClasses,
-			FExtendExtensionPointDelegate::CreateUObject(this, &ThisClass::OnAddOrRemoveExtension)
-		));
-	}
+        ExtensionPointHandles.Add(ExtensionSubsystem->RegisterExtensionPointForContext(
+                ExtensionPointTag,
+                PlayerState,
+                ExtensionPointTagMatch,
+                AllowedDataClasses,
+                FExtendExtensionPointDelegate::CreateUObject(this, &ThisClass::OnAddOrRemoveExtension)
+            ));
+    }
 }
 
 void UUIExtensionPointWidget::OnAddOrRemoveExtension(EUIExtensionAction Action, const FUIExtensionRequest& Request)
 {
-	if (Action == EUIExtensionAction::Added)
-	{
-		UObject* Data = Request.Data;
-		
-		TSubclassOf<UUserWidget> WidgetClass(Cast<UClass>(Data));
-		if (WidgetClass)
-		{
-			UUserWidget* Widget = CreateEntryInternal(WidgetClass);
-			ExtensionMapping.Add(Request.ExtensionHandle, Widget);
-		}
-		else if (DataClasses.Num() > 0)
-		{
-			if (GetWidgetClassForData.IsBound())
-			{
-				WidgetClass = GetWidgetClassForData.Execute(Data);
+    if (Action == EUIExtensionAction::Added)
+    {
+        UObject* Data = Request.Data;
 
-				// If the data is irrelevant they can just return no widget class.
-				if (WidgetClass)
-				{
-					if (UUserWidget* Widget = CreateEntryInternal(WidgetClass))
-					{
-						ExtensionMapping.Add(Request.ExtensionHandle, Widget);
-						ConfigureWidgetForData.ExecuteIfBound(Widget, Data);
-					}
-				}
-			}
-		}
-	}
-	else
-	{
-		if (UUserWidget* Extension = ExtensionMapping.FindRef(Request.ExtensionHandle))
-		{
-			RemoveEntryInternal(Extension);
-			ExtensionMapping.Remove(Request.ExtensionHandle);
-		}
-	}
+        TSubclassOf<UUserWidget> WidgetClass(Cast<UClass>(Data));
+        if (WidgetClass)
+        {
+            UUserWidget* Widget = CreateEntryInternal(WidgetClass);
+            ExtensionMapping.Add(Request.ExtensionHandle, Widget);
+        }
+        else if (DataClasses.Num() > 0)
+        {
+            if (GetWidgetClassForData.IsBound())
+            {
+                WidgetClass = GetWidgetClassForData.Execute(Data);
+
+                // If the data is irrelevant they can just return no widget class.
+                if (WidgetClass)
+                {
+                    if (UUserWidget* Widget = CreateEntryInternal(WidgetClass))
+                    {
+                        ExtensionMapping.Add(Request.ExtensionHandle, Widget);
+                        ConfigureWidgetForData.ExecuteIfBound(Widget, Data);
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        if (UUserWidget* Extension = ExtensionMapping.FindRef(Request.ExtensionHandle))
+        {
+            RemoveEntryInternal(Extension);
+            ExtensionMapping.Remove(Request.ExtensionHandle);
+        }
+    }
 }
 
 #if WITH_EDITOR
 void UUIExtensionPointWidget::ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const
 {
-	Super::ValidateCompiledDefaults(CompileLog);
+    Super::ValidateCompiledDefaults(CompileLog);
 
-	// We don't care if the CDO doesn't have a specific tag.
-	if (!HasAnyFlags(RF_ClassDefaultObject))
-	{
-		if (!ExtensionPointTag.IsValid())
-		{
-			TSharedRef<FTokenizedMessage> Message = CompileLog.Error(FText::Format(LOCTEXT("UUIExtensionPointWidget_NoTag", "{0} has no ExtensionPointTag specified - All extension points must specify a tag so they can be located."), FText::FromString(GetName())));
-			Message->AddToken(FUObjectToken::Create(this));
-		}
-	}
+    // We don't care if the CDO doesn't have a specific tag.
+    if (!HasAnyFlags(RF_ClassDefaultObject))
+    {
+        if (!ExtensionPointTag.IsValid())
+        {
+            TSharedRef<FTokenizedMessage> Message = CompileLog.Error(FText::Format(LOCTEXT("UUIExtensionPointWidget_NoTag", "{0} has no ExtensionPointTag specified - All extension points must specify a tag so they can be located."), FText::FromString(GetName())));
+            Message->AddToken(FUObjectToken::Create(this));
+        }
+    }
 }
 #endif
 
 /////////////////////////////////////////////////////
 
 #undef LOCTEXT_NAMESPACE
-
--- Plugins/UIExtension/Source/Public/Widgets/UIExtensionPointWidget.h
+++ Plugins/UIExtension/Source/Public/Widgets/UIExtensionPointWidget.h
@@ -18,49 +18,53 @@ class APlayerState;
 UCLASS()
 class UIEXTENSION_API UUIExtensionPointWidget : public UDynamicEntryBoxBase
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
+    DECLARE_DYNAMIC_DELEGATE_RetVal_OneParam(TSubclassOf<UUserWidget>, FOnGetWidgetClassForData, UObject*, DataItem);
 
-	DECLARE_DYNAMIC_DELEGATE_RetVal_OneParam(TSubclassOf<UUserWidget>, FOnGetWidgetClassForData, UObject*, DataItem);
-	DECLARE_DYNAMIC_DELEGATE_TwoParams(FOnConfigureWidgetForData, UUserWidget*, Widget, UObject*, DataItem);
+    DECLARE_DYNAMIC_DELEGATE_TwoParams(FOnConfigureWidgetForData, UUserWidget*, Widget, UObject*, DataItem);
 
-	UUIExtensionPointWidget(const FObjectInitializer& ObjectInitializer);
+    UUIExtensionPointWidget(const FObjectInitializer& ObjectInitializer);
 
-	//~UWidget interface
-	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
-	virtual TSharedRef<SWidget> RebuildWidget() override;
+    //~UWidget interface
+    virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+
+    virtual TSharedRef<SWidget> RebuildWidget() override;
 #if WITH_EDITOR
-	virtual void ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const override;
+    virtual void ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const override;
 #endif
-	//~End of UWidget interface
+    //~End of UWidget interface
 
 private:
-	void ResetExtensionPoint();
-	void RegisterExtensionPoint();
-	void RegisterExtensionPointForPlayerState(UCommonLocalPlayer* LocalPlayer, APlayerState* PlayerState);
-	void OnAddOrRemoveExtension(EUIExtensionAction Action, const FUIExtensionRequest& Request);
+    void ResetExtensionPoint();
+
+    void RegisterExtensionPoint();
+
+    void RegisterExtensionPointForPlayerState(UCommonLocalPlayer* LocalPlayer, APlayerState* PlayerState);
+
+    void OnAddOrRemoveExtension(EUIExtensionAction Action, const FUIExtensionRequest& Request);
 
 protected:
-	/** The tag that defines this extension point */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
-	FGameplayTag ExtensionPointTag;
+    /** The tag that defines this extension point */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
+    FGameplayTag ExtensionPointTag;
 
-	/** How exactly does the extension need to match the extension point tag. */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
-	EUIExtensionPointMatch ExtensionPointTagMatch = EUIExtensionPointMatch::ExactMatch;
+    /** How exactly does the extension need to match the extension point tag. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
+    EUIExtensionPointMatch ExtensionPointTagMatch = EUIExtensionPointMatch::ExactMatch;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
-	TArray<TObjectPtr<UClass>> DataClasses;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
+    TArray<TObjectPtr<UClass>> DataClasses;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="UI Extension", meta=( IsBindableEvent="True" ))
-	FOnGetWidgetClassForData GetWidgetClassForData;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="UI Extension", meta=( IsBindableEvent="True" ))
+    FOnGetWidgetClassForData GetWidgetClassForData;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="UI Extension", meta=( IsBindableEvent="True" ))
-	FOnConfigureWidgetForData ConfigureWidgetForData;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="UI Extension", meta=( IsBindableEvent="True" ))
+    FOnConfigureWidgetForData ConfigureWidgetForData;
 
-	TArray<FUIExtensionPointHandle> ExtensionPointHandles;
+    TArray<FUIExtensionPointHandle> ExtensionPointHandles;
 
-	UPROPERTY(Transient)
-	TMap<FUIExtensionHandle, TObjectPtr<UUserWidget>> ExtensionMapping;
+    UPROPERTY(Transient)
+    TMap<FUIExtensionHandle, TObjectPtr<UUserWidget>> ExtensionMapping;
 };
